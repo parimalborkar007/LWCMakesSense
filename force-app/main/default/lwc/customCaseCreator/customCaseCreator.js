f5b86d77cf0593409676823482872e78820709b5
Parimal Borkar <parimalborkar007@gmail.com>
2026-06-13 16:33:32 +0530

    customCase

--- force-app/main/default/lwc/customCaseCreator/customCaseCreator.js
+++ force-app/main/default/lwc/customCaseCreator/customCaseCreator.js
@@ -1,6 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { createRecord } from 'lightning/uiRecordApi';
-import {getObjectInfo} from 'lightning/uiObjectInfoApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+
 import CASE_OBJECT from '@salesforce/schema/Case';
 import SUBJECT_FIELD from '@salesforce/schema/Case.Subject';
 import DESCRIPTION_FIELD from '@salesforce/schema/Case.Description';
@@ -8,21 +9,38 @@ import PRIORITY_FIELD from '@salesforce/schema/Case.Priority';
 
 export default class CustomCaseCreator extends LightningElement {
 
-    subjecr = '';
+    subject = '';
     description = '';
     priority = '';
+    recordTypeId = '';
 
-    @wire(getObjectInfo, { objectApiName: CASE_OBJECT }, caseRecord)
+    @wire(getObjectInfo, { objectApiName: CASE_OBJECT })
+    caseRecord({ data, error }) {
+        if (data) {
+            const recordTypes = Object.values(data.recordTypeInfos);
 
-    get recordTypeName(){
+            const rt = recordTypes.find(rt => 
+                rt.developerName === 'SMS_Community_Cases'
+            );
 
+            if (rt) {
+                this.recordTypeId = rt.recordTypeId;
+                console.log('Record Type Found:', this.recordTypeId);
+            } else {
+                console.error('Record Type not found!');
+            }
+        }
+
+        if (error) {
+            console.error('Error fetching Case Record Type Info:', error);
+        }
     }
 
     get priorityOptions() {
         return [
-            { label: 'Low', value: 'low' },
-            { label: 'Medium', value: 'medium' },
-            { label: 'High', value: 'high' },
+            { label: 'Low', value: 'Low' },
+            { label: 'Medium', value: 'Medium' },
+            { label: 'High', value: 'High' },
         ];
     }
 
@@ -40,21 +58,28 @@ export default class CustomCaseCreator extends LightningElement {
 
     async createCase() {
 
+        if (!this.recordTypeId) {
+            console.error('Record Type ID is missing!');
+            return;
+        }
+
         const fields = {};
 
-        fields[SUBJECT_FIELD.fieldApiName] = this.subject; 
+        fields[SUBJECT_FIELD.fieldApiName] = this.subject;
         fields[DESCRIPTION_FIELD.fieldApiName] = this.description;
         fields[PRIORITY_FIELD.fieldApiName] = this.priority;
+        fields['RecordTypeId'] = this.recordTypeId;
 
-        let recordInput = {apiName: CASE_OBJECT.objectApiName, fields};
-        await createRecord(recordInput)
-        .then(record => {
+        const recordInput = {
+            apiName: CASE_OBJECT.objectApiName,
+            fields
+        };
+
+        try {
+            const record = await createRecord(recordInput);
             alert('Case created successfully with Id: ' + record.id);
-        })
-        .catch(error => {
-            console.error('Error creating case:' + error.body.message);
-        })
-
+        } catch (error) {
+            console.error('Error creating case:', error.body?.message);
+        }
     }
-
 }
